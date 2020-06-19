d1a6cb381cb1cab216f05186abfc229906ec726c
pabra <patrick+github-commit-2020@braune.xyz>
2020-06-19 23:43:58 +0200

    extend cli further

--- src/cli.ts
+++ src/cli.ts
@@ -1,53 +1,79 @@
 #!/usr/bin/env node
 
 import arg from 'arg';
-import { basename } from 'path';
+import { basename, join } from 'path';
+import { getPackageJson } from './index';
 
 const args = arg({
   // Types
   '--help': Boolean,
   '--version': Boolean,
+  '--tag': String,
 
   // Aliases
   '-h': '--help',
   '-v': '--version',
+  '-t': '--tag',
 });
 
-const showHelp = (): void => {
-  process.stdout.write(
-    `usage ${basename(process.argv[1])} [options]
+console.log('args:', args); // TODO: remove DEBUG
 
-    Options:
-      -h, --help        show this help
-      -v, --version     show version
-      -d, --debug       log debug output
-      -f, --file        path to DNS entries exporting file
-      -w, --write       actually "write" entries through INWX API
-      -i, --insane      ignore sanity checks
-    \n`,
-  );
+const getOwnPackageJson = () => {
+  try {
+    // while development, __filename is in src/ -> package.json must be one dir up
+    return getPackageJson(join(__dirname, '..'));
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+
+  try {
+    // after build, __filename is in dist/cjs/ -> package.json must be two dirs up
+    return getPackageJson(join(__dirname, '..', '..'));
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+
+  return {};
+};
+
+const getOwnVersionString = (): string => {
+  const { name, version } = getOwnPackageJson();
+  return `${name} version: ${version}`;
+};
+
+const showHelp = (stderr = false): void => {
+  const { description } = getOwnPackageJson();
+  const help = [
+    getOwnVersionString(),
+    description,
+    '',
+    'USAGE:',
+    `    ${basename(process.argv[1])} [OPTIONS] PACKAGE_NAME`,
+    '',
+    'OPTIONS:',
+    '    -h, --help',
+    '        show this help',
+    '',
+    '    -v, --version',
+    '        show version',
+    '',
+    '    -t, --tag=<TAG_NAME>',
+    '        tag of the package to look for (default: latest)',
+    '',
+    'ARGS:',
+    '    PACKAGE_NAME',
+    '        name of the package to look for',
+  ].join('\n');
+
+  process[stderr ? 'stderr' : 'stdout'].write(`${help}\n`);
 };
 
 const showVersion = (): void => {
-  let packageJson: any = {};
-
-  /* eslint-disable node/no-missing-require, @typescript-eslint/no-var-requires */
-  try {
-    packageJson = require('../package.json');
-  } catch (err1) {
-    if (err1.code !== 'MODULE_NOT_FOUND') {
-      throw err1;
-    }
-    try {
-      packageJson = require('../../package.json');
-    } catch (err2) {
-      if (err2.code !== 'MODULE_NOT_FOUND') {
-        throw err2;
-      }
-    }
-  }
-  /* eslint-enable node/no-missing-require, @typescript-eslint/no-var-requires */
-  process.stdout.write(`${packageJson.name} version: ${packageJson.version}\n`);
+  process.stdout.write(`${getOwnVersionString()}\n`);
 };
 
 if (args['--help']) {
@@ -55,5 +81,13 @@ if (args['--help']) {
 } else if (args['--version']) {
   showVersion();
 } else {
-  // TODO: add
+  const packageName = args._[0];
+
+  if (!packageName) {
+    process.stderr.write('missing PACKAGE_NAME\n\n');
+    showHelp(true);
+    throw new Error();
+  }
+
+  // const tag = args['--tag'] ?? 'latest';
 }
